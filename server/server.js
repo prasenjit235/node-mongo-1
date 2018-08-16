fdf736e07422965eedb7e855de05cab626124f4d
Prasenjit Das <prasenjit235@gmail.com>
2018-08-17 00:07:44 +0530

    first commit

---
+++ server/server.js
@@ -0,0 +1,55 @@
+require('./config/config');
+
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+
+var {mongoose} = require('./db/mongoose');
+var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
+
+var app = express();
+const port = process.env.PORT;
+
+app.use(bodyParser.json());
+
+// POST /users/signup
+app.post('/user/signup', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password','firstname','lastname']);
+  var user = new User(body);
+
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((e) => {
+    res.status(400).send(e);
+  })
+});
+
+app.post('/user/login', (req, res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
+
+app.delete('/user/logout', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send();
+  }, () => {
+    res.status(400).send();
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Started up at port ${port}`);
+});
+
+module.exports = {app};
